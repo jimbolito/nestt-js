881a991dde506b31e479f3c1da2d4552a6007ce7
jimmy <jimmycoqueyanez1991@gmail.com>
2024-01-23 00:33:32 +0100

    clase 0 y 1

---
+++ src/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
+
+
+@Module({
+
+  imports: [
+    //Variables de entorno:
+    ConfigModule.forRoot({
+      // envFilePath: `.${process.env.NODE_ENV}.env`, 
+      envFilePath: '.develop.env', 
+      isGlobal: true
+    }),
+    //Todo se va a manejar en base a las rutas que tiene el UsersModule:
+    UsersModule
+  ]
+  // controllers: [AppController],
+  // providers: [AppService],
+})
+export class AppModule { }
---
+++ src/main.ts
@@ -0,0 +1,29 @@
+import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
+import { AppModule } from './app.module';
+import * as morgan from 'morgan';
+import { CORS } from './constants';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  //usar morgan con el entorno dev
+  app.use(morgan('dev'))
+
+  //Se puede poner el prefijo api o por ejemplo para el control de versiones
+  app.setGlobalPrefix('api');
+
+  //Usando variable de entorno para el puerto: 
+  const configService = app.get(ConfigService);
+  const port = configService.get('PORT');
+  // console.log("PUERTO DE LA APLICACION: " + port );
+
+  //SEGURIDAD CORS:
+  app.enableCors(CORS);
+
+  await app.listen(3000);
+
+  //Donde corre la aplición: debe estar primero la linea anterior   await app.listen(3000);
+  console.log(`Application running on " ${await app.getUrl()}`)
+}
+bootstrap();
